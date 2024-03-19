538a5dfd7b9c7b5a24963d9665b5e3da56580a7a
Dawithabte31 <dawithabitamu31@gmail.com>
2024-03-19 23:58:13 +0300

    tailwind

--- client/src/pages/AboutH.jsx
+++ client/src/pages/AboutH.jsx
@@ -5,10 +5,10 @@ function AboutH() {
     <div className='about_club_history'>
     <div className='about_club_history_desc'>
        <div className='about_club_history_flex'>
-       <h1 className='about_h'>
-          Our Club History
+       <h1 className='about_h text-5xl mb-8'>
+        ABOUT US
        </h1>
-        <p className='about_club_history_p' >
+        <p className='about_club_history' >
  Lorem ipsum dolor sit amet consectetur adipisicing elit.
  Dolorum, laudantium eaque esse aliquid molestiae numquam
  commodi autem error dicta hic eligendi officiis quodrecus
@@ -19,12 +19,11 @@ function AboutH() {
        </p>
        </div>
        <div className='image'>
-       <img src="src\components\images\tech.jpg" alt="" />
+       <img src="src/images/tech.jpg" alt="" />
        </div>
       
     </div> 
   </div>
-
   )
 }
 
