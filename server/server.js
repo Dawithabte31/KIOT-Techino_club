4e21e7380ea39f5f90f7bcf09851a095575a4781
Dawithabte31 <dawithabitamu31@gmail.com>
2024-04-17 04:01:06 +0300

    checking message added to server

--- server/server.js
+++ server/server.js
@@ -69,3 +69,6 @@ const port = process.env.PORT || 3000;
 app.listen(port,() => {
   console.log(`Server running on port ${port}`);
 });
+app.get("/", (req, res) => {
+  res.send("Deployment successful! Your server is up and running.");
+});
